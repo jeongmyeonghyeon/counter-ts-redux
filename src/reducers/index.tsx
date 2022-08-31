4cccd9050dddec6ca2158cf4e8d46af9950f79bd
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-31 20:36:48 +0900

    하나의 앱에서는 하나의 Reducer - counter/todos reducers 분리 후, combineReducers 적용

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,14 +1,10 @@
-const counter = (state = 0, action: { type: string }) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-      break;
-    case "DECREMENT":
-      return state - 1;
-      break;
-    default:
-      return state;
-  }
-};
+import { combineReducers } from "redux";
+import counter from "./counter";
+import todos from "./todos";
 
-export default counter;
+const rootReducer = combineReducers({
+  counter,
+  todos,
+});
+
+export default rootReducer;
---
+++ src/reducers/todos.tsx
@@ -0,0 +1,10 @@
+const todos = (state = {}, action: { type: string }) => {
+  switch (action.type) {
+    case "TODOS":
+      break;
+    default:
+      return state;
+  }
+};
+
+export default todos;
