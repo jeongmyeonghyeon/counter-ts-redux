2fa6ccf1421394650fe0a04150e50820bf164df6
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-31 20:55:55 +0900

    Todo UI 및 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 
 type Props = {
@@ -6,11 +7,26 @@ type Props = {
 };
 
 function App({ onIncrement, onDecrement }: Props) {
+  const [todoValue, setTodoValue] = useState("");
+
+  const handleTodoOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodoValue("");
+  };
   return (
     <div className="App">
       {/* Clicked: {value} times */}
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
+
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleTodoOnChange} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
