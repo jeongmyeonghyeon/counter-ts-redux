b9d9835845a61e4b1d10d548468fca800b3021d2
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-31 23:33:58 +0900

    fetchPosts ES6로  수정

--- src/actions/posts.tsx
+++ src/actions/posts.tsx
@@ -1,13 +1,21 @@
 import axios from "axios";
 
-const fetchPosts = (): any => {
-  return async function fetchPostsThunk(dispatch: any, getState: any) {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-    console.log("response.data", response.data);
-    dispatch({ type: "FETCH_POSTS", payload: response.data });
-  };
+// const fetchPosts: any = () => {
+//   return async function fetchPostsThunk(dispatch: any, getState: any) {
+//     const response = await axios.get(
+//       "https://jsonplaceholder.typicode.com/posts"
+//     );
+//     console.log("response.data", response.data);
+//     dispatch({ type: "FETCH_POSTS", payload: response.data });
+//   };
+// };
+
+// ES6
+const fetchPosts = (): any => async (dispatch: any, getState: any) => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
 export { fetchPosts };
